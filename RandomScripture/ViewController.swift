d82dd5d5274b916a79259a860f90222543040bc3
Ryan Walker <ryanwalker7@gmail.com>
2017-05-13 19:27:16 -0700

    Swipe through scriptures

--- RandomScripture/ViewController.swift
+++ RandomScripture/ViewController.swift
@@ -8,47 +8,85 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
 
+    @IBOutlet var scriptureLabel: UILabel!
     
-    let randomScriptures = ["random 1", "random 2", "random 3"]
+    @IBOutlet var theView: UIView!
+    let randomScriptures = ["random 1", "random 2", "random 3", "random 4", "random 5"]
+
+    let swipeRightRecognizer = UISwipeGestureRecognizer()
+    let swipeLeftRecognizer = UISwipeGestureRecognizer()
+    
     var index = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        swipeRightRecognizer.direction = .right
+        swipeLeftRecognizer.direction = .left
         
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(handleRefresh(refreshControl:)), for: UIControlEvents.valueChanged)
-        
-        self.refreshControl = refreshControl
-        
+        swipeRightRecognizer.addTarget(self, action: #selector(swiped))
+        swipeLeftRecognizer.addTarget(self, action: #selector(swiped))
+
+        theView.addGestureRecognizer(swipeRightRecognizer)
+        theView.addGestureRecognizer(swipeLeftRecognizer)
+
+        theView.isUserInteractionEnabled = true
+    }
+
+    
+    func swiped(gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case UISwipeGestureRecognizerDirection.right:
+            print("Right...")
+            prevScrip()
+        case UISwipeGestureRecognizerDirection.left:
+            nextScrip()
+            print("Left...")
+        default:
+            print("Who cares...")
+        }
+        print("Swiped i guess")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nextScrip()
+    }
+    
+    func nextScrip() {
+        if index >= 4 {
+            index = 0
+        } else {
+            index += 1
+        }
+        scriptureLabel.text = randomScriptures[index]
+    }
+    
+    func prevScrip() {
+        if index <= 0 {
+            index = 4
+        } else {
+            index -= 1
+        }
+        scriptureLabel.text = randomScriptures[index]
     }
     
     func handleRefresh(refreshControl: UIRefreshControl) {
         print("refresh")
-        self.tableView.reloadData()
-        refreshControl.endRefreshing()
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1;
-    }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel?.text = randomScriptures[index]
-        cell.detailTextLabel?.text = "detailed message i guess"
-        
-        index = (index >= 2 ? 0 : index + 1)
-
-        return cell
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        
+    }
 }
 
