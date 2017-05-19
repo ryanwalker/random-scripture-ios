967b8981b11d848fd08b66c5416ce3c4480c99eb
Ryan Walker <ryanwalker7@gmail.com>
2017-05-19 15:01:51 -0700

    Add reference and animations

---
+++ RandomScripture/Scripture.swift
@@ -0,0 +1,16 @@
+//
+//  Scripture.swift
+//  RandomScripture
+//
+//  Created by Ryan Walker on 5/19/17.
+//  Copyright © 2017 Ryan Walker. All rights reserved.
+//
+
+import Foundation
+
+public struct Scripture {
+    
+    let reference: String!
+    let scripture: String!
+    
+}
--- RandomScripture/ViewController.swift
+++ RandomScripture/ViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var scriptureLabel: UILabel!
+    @IBOutlet var scriptureReference: UILabel!
     
     @IBOutlet var theView: UIView!
-    let randomScriptures = ["And it came to pass ... 1",
-                            "that I nephi ... 2",
-                            "said unto my father ... 3",
-                            "I will go ... 4",
-                            "and do ... 5"]
+    let randomScriptures =
+        [Scripture(reference: "1 Nephi 3:7", scripture: "And it came to pass ... 1"),
+         Scripture(reference: "1 Nephi 3:8", scripture: "that I nephi ... 2"),
+         Scripture(reference: "1 Nephi 3:9", scripture: "that I nephi ... 2"),
+         Scripture(reference: "Helaman 3:2", scripture: "that I nephi ... 2"),
+         Scripture(reference: "D&C 4:1", scripture: "and do the thing which the lord hath commanded, for I know that the lord giveth no commandment unto the children of men save he shall prepare a way for them ... 5")
+    ]
 
     let swipeRightRecognizer = UISwipeGestureRecognizer()
     let swipeLeftRecognizer = UISwipeGestureRecognizer()
@@ -61,7 +64,12 @@ class ViewController: UIViewController {
             index += 1
         }
         scriptureLabel.swipeAnimation(transitionType: kCATransitionFromRight)
-        scriptureLabel.text = randomScriptures[index]
+        scriptureReference.swipeAnimation(transitionType: kCATransitionFromRight)
+
+        let scripture = randomScriptures[index]
+        scriptureReference.text = scripture.reference
+        scriptureLabel.text = scripture.scripture
+        
     }
     
     func prevScrip() {
@@ -71,7 +79,12 @@ class ViewController: UIViewController {
             index -= 1
         }
         scriptureLabel.swipeAnimation(transitionType: kCATransitionFromLeft)
-        scriptureLabel.text = randomScriptures[index]
+        scriptureReference.swipeAnimation(transitionType: kCATransitionFromLeft)
+
+        let scripture = randomScriptures[index]
+        scriptureReference.text = scripture.reference
+        scriptureLabel.text = scripture.scripture
+
     }
     
 
